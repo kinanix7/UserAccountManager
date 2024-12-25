93c6f661c2d73a05fb386dc73cf537dd55d9ca00
kinanix7 <kinanix7279x@gmail.com>
2024-12-25 14:55:40 +0100

    fix code

--- UserAccountManager/src/Main.java
+++ UserAccountManager/src/Main.java
@@ -1,39 +1,41 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class Main {
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
 
         UserManagement userManagement = new UserManagement();
-      Scanner scanner= new Scanner(System.in);
+        Scanner scanner= new Scanner(System.in);
+   userManagement.roles.add(new Role(1, "Admin"));
 
 
 
-      while (true){
-       System.out.println("\n=== Menu de Gestion des Utilisateurs ===");
-       System.out.println("1. Ajouter ");
-       System.out.println("2. Rechercher ");
-       System.out.println("3. Modifier ");
-       System.out.println("4. Supprimer ");
-       System.out.println("5. Afficher ");
-       System.out.println("0. Quitter");
 
-         System.out.print("Enter votre choix : ");
-         String choix = scanner.nextLine();
+        while (true){
+            System.out.println("\n=== Menu de Gestion des Utilisateurs ===");
+            System.out.println("1. Ajouter ");
+            System.out.println("2. Rechercher ");
+            System.out.println("3. Modifier ");
+            System.out.println("4. Supprimer ");
+            System.out.println("5. Afficher ");
+            System.out.println("0. Quitter");
 
-         switch (choix){
-          case "1":userManagement.ajouterUtilisateur(); break;
-          case "2":break;
-          case "3": break;
-          case "4":userManagement.removeUser(); break;
-          case "5":userManagement.displayUsers(); break;
-          case "0": System.out.println("Goodbye!"); System.exit(0); break;
-            default:
-             System.out.println("Choix invalide!");
-         }
-      }
+            System.out.print("Enter votre choix : ");
+            String choix = scanner.nextLine();
+
+            switch (choix){
+                case "1":userManagement.ajouterUtilisateur(); break;
+                case "2":userManagement.searchUser();break;
+                case "3":userManagement.modifierUtilisateur(); break;
+                case "4":userManagement.removeUser(); break;
+                case "5":userManagement.displayUsers(); break;
+                case "0": System.out.println("Goodbye!"); System.exit(0); break;
+                default:
+                    System.out.println("Choix invalide!");
+            }
+        }
 
 
 
- }}
+    }}
--- UserAccountManager/src/UserManagement.java
+++ UserAccountManager/src/UserManagement.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,48 +8,119 @@ public class UserManagement {
     Scanner scanner = new Scanner(System.in);
 
     public void ajouterUtilisateur(){
-        System.out.print("Enter l'ID de l'utilisateur : ");
+        System.out.print("Enter l'ID : ");
         int id = scanner.nextInt();
         scanner.nextLine();
-        System.out.print("Enter le nom de l'utilisateur : ");
+        System.out.print("Enter le nom : ");
 
         String nom = scanner.nextLine();
-        System.out.print("Enter l'age de l'utilisateur : ");
+        System.out.print("Enter l'age : ");
         int age = scanner.nextInt();
-        System.out.print("Enter l'email de l'utilisateur : ");
+        System.out.print("Enter l'email : ");
         String email = scanner.next();
         scanner.nextLine();
 
-        System.out.print("Enter le mot de passe de l'utilisateur : ");
+        System.out.print("Enter le mot de passe : ");
         String motDePasse = scanner.nextLine();
 
-        System.out.print("Enter le role de l'utilisateur : ");
+        System.out.print("Enter le role  : ");
         String rolename = scanner.nextLine();
         Role role = new Role(id, rolename);
-
-
         utilisateurs.add(new Utilisateur(id, age, nom, email, motDePasse, role));
 
-        System.out.println("Utilisateur ajouté avec succès!");
     }
-    public void removeUser(){
+
+    public void modifierUtilisateur(){
         if (utilisateurs.isEmpty()){
-            System.out.println("Aucun utilisateur trouvé!");
+        System.out.println("Aucun utilisateur trouve!");
+    }
+        System.out.print("Enter l'ID de l'utilisateur à modifier : ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("1. Modifier le nom `\n` 2. Modifier l'age `\n` 3. Modifier l'email `\n` 4. Modifier le role ");
+        System.out.print("Enter your choice: ");
+        String choix = scanner.nextLine();
+        scanner.nextLine();
+        for (Utilisateur utilisateur : utilisateurs){
+            if (utilisateur.getId() == id){
+
+                switch (choix){
+                    case "1":
+                        System.out.print("Enter le nouveau nom : ");
+                        utilisateur.setNom(scanner.nextLine());
+
+
+                        break;
+                    case "2":
+                        System.out.print("Enter le nouvel age : ");
+                        utilisateur.setAge(scanner.nextInt());
+                        break;
+                    case "3":
+                        System.out.print("Enter le nouvel email : ");
+                        utilisateur.setEmail(scanner.nextLine());
+                        break;
+                    case "4":
+                        System.out.print("Enter le nouveau role : ");
+                        String rolename = scanner.nextLine();
+                        Role role = new Role(id, rolename);
+                        break;
+                    default:
+                        System.out.println("Choix invalide!");
+
+                }
+            }
+        }
+
+
+
+
+
+    }
+
+
+    public void removeUser(){
+        System.out.print("Enter l'ID de l'utilisateur à supprimer : ");
+        int id = scanner.nextInt();
+        if (utilisateurs.isEmpty()){
+            System.out.println("Aucun utilisateur trouve!");
             return;
         }
-        utilisateurs.removeIf(utilisateur -> {
-            System.out.print("Enter l'ID de l'utilisateur à supprimer : ");
-            int id = scanner.nextInt();
-            return utilisateur.getId() == id;
-        });
+
+        utilisateurs.removeIf(utilisateur -> utilisateur.getId() == id);
     }
 
 
+    public void searchUser(){
+        String valueSearch;
+        ArrayList<Utilisateur> usersFound = new ArrayList<>();
+        System.out.print("Enter Email ou le nom   de l'utilisateur à rechercher : ");
+        valueSearch = scanner.nextLine();
+
+        for (Utilisateur utilisateur : utilisateurs){
+            if (utilisateur.getNom().contains(valueSearch)
+                    || utilisateur.getEmail().contains(valueSearch)
+                    || utilisateur.getRole().getRoleName().contains(valueSearch)){
+
+                usersFound.add(utilisateur);
+            }
+        }
+
+        if (usersFound.isEmpty()){
+            System.out.println("Aucun utilisateur trouve!");
+
+        }else {
+            for (Utilisateur utilisateur : usersFound){
+                System.out.println(utilisateur);
+            }
+        }
+
+
+    }
 
 
     public void displayUsers(){
         if (utilisateurs.isEmpty()){
-            System.out.println("Aucun utilisateur trouvé!");
+            System.out.println("Aucun utilisateur trouve!");
             return;
         }
         for (Utilisateur utilisateur : utilisateurs){
@@ -56,4 +128,4 @@ public class UserManagement {
         }
     }
 
-  }
+}
